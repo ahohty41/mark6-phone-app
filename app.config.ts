059ab411672b3d3962e221fd4c96904dfe5eea08
ahohty41 <alex.ho.hty@gmail.com>
2026-03-06 23:01:40 +0800

    feat: update versionCode 4 and version 1.0.4

--- app.config.ts
+++ app.config.ts
@@ -24,7 +24,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   },
   android: {
     package: 'com.ahohty.marksixluckydraw',
-    versionCode: 3,
+    versionCode: 4,
     adaptiveIcon: {
       foregroundImage: './assets/adaptive-icon.png',
       backgroundColor: '#450a0a',
